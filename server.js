d7f2ee7de8fa97c238ebe3baa945eec3bf0b6f97
Felipe Albuquerque <dev.felipealbuquerque@gmail.com>
2024-11-24 16:29:49 -0400

    retirei um console.log e adcionei comentários ao código

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-import express from "express";
-import routes from "./src/routes/postsRoutes.js";
+import express from "express"; // Importa o framework Express para criar a aplicação web
+import routes from "./src/routes/postsRoutes.js"; // Importa as rotas definidas em postsRoutes.js
 
-// Cria uma instância do Express
+// Cria uma aplicação Express
 const app = express();
+// Configura o diretório "uploads" para servir arquivos estáticos
 app.use(express.static("uploads"));
+// Utiliza as rotas definidas em postsRoutes.js
 routes(app)
 
 // Inicia o servidor na porta 3000 e exibe uma mensagem no console
 app.listen(3000, () => {
   console.log("Servidor escutando...");
-  console.log("Meu primeiro servidor!");
 });
 
 
